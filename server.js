835e3c6133a496a252d0310822d3bdf86d393eab
Sornnacha Buranapongwattana <149521515+maxsornnacha@users.noreply.github.com>
2024-05-21 11:55:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -76,8 +76,7 @@ app.use(
     cookie:{
       secure: process.env.NODE_ENV === 'production', // only transmit cookie over https
       maxAge: 3 * 60 * 60 * 1000, // 3 hours in milliseconds
-      sameSite: 'lax',
-      httpOnly: process.env.NODE_ENV === 'production', 
+      sameSite: 'lax'
     }
   })
 );
